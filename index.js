9eaafb027c42e053dbf7ffa16180a47df015f5ed
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-07-10 22:49:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('question1').oninput = function () {
+    document.getElementById('output1').innerHTML = this.value;
+}
+
+document.getElementById('question2').oninput = function () {
+    document.getElementById('output2').innerHTML = this.value;
+}
